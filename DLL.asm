61a8024c1e77b268203bb3514fb1c86b1eb46e4f
RnMor777 <ryan.morganti@trojans.dsu.edu>
2021-10-14 15:31:42 -0500

    Operational and working

--- DLL.asm
+++ DLL.asm
@@ -1,11 +1,16 @@
 segment .data
-    printformat     db  "%d", 10,  0
+    printformat     db  "%d -> ", 0, 0
+    printform2      db  "%d", 10, 0
+    scanformat      db  "%d", 0
+    printNl         db  10, 0
 
 segment .bss
-    dllPointer      resd 1    
+    dllPointer      resd    1    
+    numIn           resd    1
 
 segment .text
     extern    printf
+    extern    scanf
     extern    malloc
     global    main
 
@@ -13,27 +18,62 @@ main:
     enter   0, 0
     pusha
 
-
     call    makelist
     mov     DWORD[dllPointer], eax
 
+    topScan: 
+    push    numIn
+    push    scanformat
+    call    scanf
+    add     esp, 8 
+
+    cmp     DWORD[numIn], 0
+    jle     endScan
+        push    DWORD[numIn]
+        push    DWORD[dllPointer]
+        call    insertSorted
+        add     esp, 8
+        jmp     topScan
+    endScan:
+    jmp     bot
+
+
     push    10 
     push    DWORD[dllPointer]
-    call    insertSorted
+    ;call    insertSorted
+    ;call    insertBack
+    call    insertFront
     add     esp, 8
 
-    push    15
+    push    14
     push    DWORD[dllPointer]
-    call    insertSorted
+    ;call    insertSorted
+    ;call    insertBack
+    call    insertFront
     add     esp, 8
 
     push    12
     push    DWORD[dllPointer]
-    call    insertSorted
+    ;call    insertSorted
+    ;call    insertBack
+    call    insertFront
     add     esp, 8
+
+    push    30
+    push    DWORD[dllPointer]
+    ;call    insertSorted
+    ;call    insertBack
+    call    insertFront
+    add     esp, 8
+
+bot:
 
     push    DWORD[dllPointer]
     call    printerF
+    add     esp, 4
+
+    push    DWORD[dllPointer]
+    call    printerR
     add     esp, 4
 
     popa
@@ -81,7 +121,7 @@ insertSorted: ; void insertSorted (struct dllheader *L, int val)
     mov     eax, DWORD[ebp+8]
     mov     eax, DWORD[eax+8]
     test    eax, eax
-    jne     insSortElse
+    jnz     insSortElse
         push    ebx
         push    DWORD[ebp+8]
         call    insertFront
@@ -91,7 +131,8 @@ insertSorted: ; void insertSorted (struct dllheader *L, int val)
         mov     eax, DWORD[ebp+8]
         mov     eax, DWORD[eax] 
         insSortTop:
-        cmp     DWORD[eax], 0
+        ;cmp     DWORD[eax], 0
+        cmp     eax, 0
         je      insSortBot
             cmp     ebx, DWORD[eax+8]
             jg      insSortIf
@@ -103,6 +144,7 @@ insertSorted: ; void insertSorted (struct dllheader *L, int val)
                 jmp     insSortEnd
             insSortIf:
             mov     eax, DWORD[eax] 
+            jmp     insSortTop
         insSortBot:
         push    ebx
         push    DWORD[ebp+8]
@@ -160,13 +202,16 @@ insertBack: ; void insertBack (struct dllheader *L, int num)
         mov     DWORD[eax+4], ecx
         jmp     insBackEnd
     insBackElse: 
-        ;mov     ebx, DWORD[ebx]
+        mov     ebx, DWORD[ebp+8]
+        mov     ebx, DWORD[ebx+4]
         mov     DWORD[ebx], ecx
+        mov     eax, DWORD[eax+4]
         mov     DWORD[ecx+4], eax
-        add     DWORD[ecx+4], 4
-        mov     ebx, DWORD[eax+4]
-        mov     DWORD[ebx], ecx
+
+        mov     ebx, DWORD[ebp+8]
+        mov     DWORD[ebx+4], ecx
     insBackEnd:
+    mov     eax, DWORD[ebp+8]
     inc     DWORD[eax+8]
 
     mov     esp, ebp
@@ -189,15 +234,18 @@ insertAfter: ; void insertAfter (struct dllheader *L, int num, struct lnode *p)
         push    DWORD[ebp+12]
         call    makenode
         add     esp, 4
+        mov     ecx, eax
 
         mov     ebx, DWORD[ebp+16]
-        mov     DWORD[eax+4], ebx
+        mov     DWORD[ecx+4], ebx
         mov     ebx, DWORD[ebx]
-        mov     DWORD[eax], ebx
+        mov     Dword[ecx], ebx
 
-        mov     DWORD[ebx+4], eax
+        mov     ebx, DWORD[ebx+4]
+        mov     DWORD[ebx], ecx
         mov     ebx, DWORD[ebp+16]
-        mov     DWORD[ebx], eax
+        mov     DWORD[ebx], ecx
+
         mov     ebx, DWORD[ebp+8]
         inc     DWORD[ebx+8]  
     insAfterEnd:
@@ -226,11 +274,14 @@ insertBefore: ; void insertBefore (struct dllheader *L, int num, structlode *p)
         mov     eax, DWORD[ebp+16]
         mov     DWORD[ecx], eax
         mov     ebx, DWORD[eax+4]
-        mov     DWORD[eax+4], ebx
+        mov     DWORD[ecx+4], ebx
 
-        mov     DWORD[ebx], ecx
-        mov     ebx, DWORD[eax+4]
-        mov     DWORD[ebx], ecx
+        mov     eax, DWORD[ebp+16]
+        mov     eax, DWORD[eax+4]
+        mov     DWORD[eax], ecx
+        mov     eax, DWORD[ebp+16]
+        mov     DWORD[eax+4], ecx
+        
         mov     ebx, DWORD[ebp+8]
         inc     DWORD[ebx+8]
     insBeforeEnd:
@@ -255,7 +306,13 @@ printerF: ; void printerF (struct dllheader *alist)
     cmp     eax, 0
     je      printFBot
         push    DWORD[eax+8]
-        push    printformat
+        mov     ebx, DWORD[eax]
+        neg     ebx
+        sbb     ebx, ebx
+        inc     ebx
+        shl     ebx, 3
+        lea     ebx, [printformat+ebx]
+        push    ebx
         call    printf
         add     esp, 8
         mov     eax, DWORD[ebp-4]
@@ -269,5 +326,41 @@ printerF: ; void printerF (struct dllheader *alist)
     ret
 
 printerR: ; void printerR (struct dllheader *alist)
+    push    ebp
+    mov     ebp, esp
+
+    sub     esp, 4
+
+    mov     eax, DWORD[ebp+8]
+    mov     eax, DWORD[eax+4]
+    mov     DWORD[ebp-4], eax
+    
+    printRTop:
+    mov     eax, DWORD[ebp-4]
+    cmp     eax, 0
+    je      printRBot
+        push    DWORD[eax+8]
+        mov     ebx, DWORD[eax+4]
+        neg     ebx
+        sbb     ebx, ebx
+        inc     ebx
+        shl     ebx, 3
+        lea     ebx, [printformat+ebx]
+        push    ebx
+        call    printf
+        add     esp, 8
+        mov     eax, DWORD[ebp-4]
+        mov     eax, DWORD[eax+4]
+        mov     DWORD[ebp-4], eax
+        jmp     printRTop
+    printRBot: 
+
+    push    printNl
+    call    printf
+    add     esp, 4
+
+    mov     esp, ebp
+    pop     ebp
+    ret
 
 ; vim:ft=nasm
